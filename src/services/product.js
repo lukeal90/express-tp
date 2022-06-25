11288bb88da59c2a26a8e2e2115f522f8f2d4303
lukeal90 <lainolucas90@gmail.com>
2022-06-25 14:23:28 -0300

    add update

--- src/services/product.js
+++ src/services/product.js
@@ -41,17 +41,16 @@ class ProductService{
     }    
     
     static async updateProduct(producto, id) {
-        // const productos = await this.getAll();
-        // let id = 1;
-    
-        // if(productos.length > 0){
-        //     id = productos[productos.length - 1].id + 1;
-        // }
+        const productos = await this.getAll();
+        const produIndex = productos.findIndex( produ => produ.id == id);
+        console.log(produIndex)
+        if(produIndex == -1){
+            return {"msg" : `No existe el producto con el ID: ${id}`};
+        }
 
-        // const newProducto = {...producto, id}
-        // productos.push(newProducto);
-        // await fs.writeFile(path.resolve(__dirname, pathFile), JSON.stringify(productos));
-        // return {"msg" : `Se agrego el producto con el ID: ${id}`};
+        productos[produIndex] = {...producto, id};
+        await fs.writeFile(path.resolve(__dirname, pathFile), JSON.stringify(productos));
+        return {"msg" : `Se actualizo el producto con el ID: ${id}`};
     }   
 
     static async deleteProduct(id) {
